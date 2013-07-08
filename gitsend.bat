29c2b12c523cc625431dea691ae4fa742ce18211
jjinkou2 <jjinkou2@yahoo.fr>
2013-07-08 08:01:20 +0200

    shell2

--- gitsend.bat
+++ gitsend.bat
@@ -1,3 +1,3 @@
-cp "C:\Program Files (x86)\Kangoku Senkan\test\newtest.ks" .
-git commit -m %1 newtest.ks
+cp "C:/Program Files (x86)/Kangoku Senkan/test/newgame.ks" .
+git commit -m %1 newgame.ks
 git push
